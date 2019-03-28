f6d7d8f4b8b9227b2a4104ae2ba7f20e69d7056b
kennerus <31510182+kennerus@users.noreply.github.com>
2019-03-28 16:33:10 +0300

    Revert "Revert "Vacancy form""

---
+++ src/models/Advert.js
@@ -0,0 +1,15 @@
+export default {
+    post: null,
+    duties: null,
+    TypeOfEmployment: null,
+    Schedule: null,
+    salaryFrom: null,
+    salaryBefore: null,
+    qualificationRequirements: null,
+    experience: null,
+    education: null,
+    workingConditions: null,
+    vacancyVideo: null,
+    officeAdress: null,
+    houseNumber: null,
+}
